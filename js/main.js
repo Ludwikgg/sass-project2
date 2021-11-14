be6ab69abeb56372650829c2a9d0c9f833d675f9
Damian <damian.ludwig83@gmail.com>
2021-11-14 02:35:58 +0100

    added js events

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const navLink = document.querySelectorAll('.nav__link');
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
+
+    navLink.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active');
+            navBtn.classList.remove('is-active');
+        })
+    })
 }
 
 navBtn.addEventListener('click', handleNav);
@@ -15,5 +23,3 @@ const handleCurrentYear = () => {
 }
 
 handleCurrentYear();
-
-// DOKONCZYC NAVIWGACJE. KLIKANIE W LINKI NIE ZAMYKA NAVIGACJI I NIE PROWADZI DO NICZEGO
